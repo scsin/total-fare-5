c525f05d8021e141fc5e6da3c358b60f00a2bcd5
Sara Sin <saracsin26@gmail.com>
2019-10-23 17:10:55 -0300

    test ok

---
+++ main.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from datetime import time
+from collections import OrderedDict
+
+records = [
+    {'source': '48-996355555', 'destination': '48-666666666', 'end': 1564610974, 'start': 1564610674},
+    {'source': '41-885633788', 'destination': '41-886383097', 'end': 1564506121, 'start': 1564504821},
+    {'source': '48-996383697', 'destination': '41-886383097', 'end': 1564630198, 'start': 1564629838},
+    {'source': '48-999999999', 'destination': '41-885633788', 'end': 1564697158, 'start': 1564696258},
+    {'source': '41-833333333', 'destination': '41-885633788', 'end': 1564707276, 'start': 1564704317},
+    {'source': '41-886383097', 'destination': '48-996384099', 'end': 1564505621, 'start': 1564504821},
+    {'source': '48-999999999', 'destination': '48-996383697', 'end': 1564505721, 'start': 1564504821},
+    {'source': '41-885633788', 'destination': '48-996384099', 'end': 1564505721, 'start': 1564504821},
+    {'source': '48-996355555', 'destination': '48-996383697', 'end': 1564505821, 'start': 1564504821},
+    {'source': '48-999999999', 'destination': '41-886383097', 'end': 1564610750, 'start': 1564610150},
+    {'source': '48-996383697', 'destination': '41-885633788', 'end': 1564505021, 'start': 1564504821},
+    {'source': '48-996383697', 'destination': '41-885633788', 'end': 1564626000, 'start': 1564647600}
+]
+
+PERMANENT_FARE = 0.36
+DAY_RATE = 0.09
+
+get_source = list(map(lambda elem: elem['source'], records))
+same_source = list(dict.fromkeys(get_source))
+
+get_ends = list(map(lambda elem: elem['end'], records))
+get_starts = list(map(lambda elem: elem['start'], records))
+
+hour_starts = list(map(lambda elem: datetime.fromtimestamp(elem).hour, get_starts))
+print(hour_starts)
+
+duration_call_in_seconds = list(map(lambda elemEnd, elemStart:
+    elemEnd - elemStart, get_ends, get_starts))
+print(duration_call_in_seconds)
+
+total_fare = list(map(lambda elemStart, elemDuration:
+    (PERMANENT_FARE + (DAY_RATE * (elemDuration/60)))
+    if (elemStart > 6 and elemStart < 22)
+    else PERMANENT_FARE,
+    hour_starts, duration_call_in_seconds))
+print(total_fare)
+
+def classify_by_phone_number(records):
+    get_infos = {}
+
+    for record in records:
+        number = record.get('source', 0)
+        get_infos[number] = (get_infos.get(number, 0.0) + total_fare[records.index(record)])
+    
+    get_infos = OrderedDict(sorted(get_infos.items(), key=lambda kv: kv[1], reverse=True))
+
+    records_answer = []
+    for number, total in get_infos.items():
+        records_answer.append({'source': number, 'total': round(total, 2)})
+        
+    print (records_answer)
+
+classify_by_phone_number(records)
